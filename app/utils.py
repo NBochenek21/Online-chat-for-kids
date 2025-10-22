9a170e07a563e74702283961ff7ebf8f450635cb
AntoniPokrzywa <antonipokrzywa@gmail.com>
2025-10-22 11:47:13 +0200

    added blueprint for llm calls

---
+++ app/utils.py
@@ -0,0 +1,9 @@
+def call_llm(question: str) -> str:
+    """ Generates api call to llm and includes question
+
+    Args:
+        question (str): Question to be answered by llm, asked by a kid
+    """
+
+    answer = "todo"
+    return answer
