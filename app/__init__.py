688d50c23045fdbb2d7c732df5dcc9a253c6c2e3
AntoniPokrzywa <antonipokrzywa@gmail.com>
2025-10-22 12:57:04 +0200

    integrated ai with whisper on mp3 uploads

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 import os
 from werkzeug.utils import secure_filename
+from transcribe import transcribe_audio
+from utils import call_llm
 UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
@@ -18,7 +20,10 @@ def upload():
     filename = secure_filename(file.filename)
     filepath = os.path.join(UPLOAD_FOLDER, filename)
     file.save(filepath)
+    question = transcribe_audio(filepath)
+    answer = call_llm(question=question)
+    return answer
 
-              
+   
 if __name__ == '__main__':
     app.run(debug=True)
